40a3e62a7ba019f86dd81256ef9524a468b3fb4d
Mitchell Wright <pilotmitchw@gmail.com>
2012-01-22 16:48:32 -0700

    Completed rubywarrior level 6.

--- project02/rubywarrior/rubi-beginner/player.rb
+++ project02/rubywarrior/rubi-beginner/player.rb
@@ -1,22 +1,35 @@
 class Player
   def play_turn(warrior)
 
+  	if(warrior.feel(:backward).wall?)
+  		@reached_back = true
+  	end
+
   	if not @health_at_end_turn.nil? and @health_at_end_turn > warrior.health
   		@being_attacked = true
   	else
   		@being_attacked = false
   	end
 
-  	if not warrior.feel.empty?
-	  	if warrior.feel.captive?
-	  		warrior.rescue!
+  	if not @reached_back.nil?
+  		@direction = :forward
+  	else
+  		@direction = :backward
+  	end
+  		
+  	if not warrior.feel(@direction).empty?
+	  	if warrior.feel(@direction).captive?
+	  		warrior.rescue!(@direction)
 	  	else
-	  		warrior.attack!
+	  		warrior.attack!(@direction)
 	  	end  	
 	elsif warrior.health < 20 and not @being_attacked
 		warrior.rest!
+	elsif warrior.health < 11 and @being_attacked
+		@direction = :backward
+		warrior.walk!(@direction)
 	else
-    	warrior.walk!
+    	warrior.walk!(@direction)
     end
 
     @health_at_end_turn = warrior.health
