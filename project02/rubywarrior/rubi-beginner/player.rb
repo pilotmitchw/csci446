e27b1a2fed83374a7e78f05e9e8ef61fb3d2146c
Mitchell Wright <pilotmitchw@gmail.com>
2012-01-22 16:35:27 -0700

    Completed rubywarrior level 4.

--- project02/rubywarrior/rubi-beginner/player.rb
+++ project02/rubywarrior/rubi-beginner/player.rb
@@ -1,11 +1,20 @@
 class Player
   def play_turn(warrior)
+
+  	if not @health_at_end_turn.nil? and @health_at_end_turn > warrior.health
+  		@being_attacked = true
+  	else
+  		@being_attacked = false
+  	end
+
   	if not warrior.feel.empty?
-	  	warrior.attack!
-	elsif warrior.health < 15
+	  	warrior.attack!  	
+	elsif warrior.health < 20 and not @being_attacked
 		warrior.rest!
 	else
     	warrior.walk!
     end
+
+    @health_at_end_turn = warrior.health
   end
 end
