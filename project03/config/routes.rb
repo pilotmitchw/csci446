552901f57f8e87cfafc42cd5088d8a2b546fa792
Mitchell Wright <pilotmitchw@gmail.com>
2012-02-05 22:49:08 -0700

    Iteration I2 complete. Authenticating users.

--- project03/config/routes.rb
+++ project03/config/routes.rb
@@ -1,4 +1,12 @@
 Project03::Application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'login' => :destroy
+  end
+
   resources :users
 
   resources :orders
