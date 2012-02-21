25bbc384b47163112182164b0c8a2caed415a57a
Mitchell Wright <pilotmitchw@gmail.com>
2012-02-20 23:18:52 -0700

    Working on enforcing requirements in the database (no author name of Pat) and styling.

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+	validates :title, :author_name, :body, :created_at, :times_edited, presence: true
+
+	validates_exclusion_of :author_name, :in => %w{Pat pat}, :message => " cannot be 'Pat'."
 end
