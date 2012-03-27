e4b5a3b025d4149a22e04f46a5c3e65e424864cd
Mitchell Wright <pilotmitchw@gmail.com>
2012-03-27 14:10:27 -0600

    Added project 06 boilerplate application

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '3553a27f08bac1e9ad0518e233312e7c414db82bd3b1b4350eeea730c8584e7e2bcdfbc8e098f3f52511f519eb983c6f0ca76af415b940732483c0ecc473cdb3'
