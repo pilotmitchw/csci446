8741537b988e05a5d0f96bc8c67be31b27954338
pilotmitchw@gmail.com <mitch@mitch-VirtualBox.(none)>
2012-03-28 15:33:44 -0600

    Starting over. For real this time.

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '59c336f10a72eaf89648cfa460887700ee38ec7ee076befbf7b49785d93f9a47461426504edb455189d041ae4ec6728f9eaaf6f069c6b11e7be912691b80206e'
