f8aab7cc1f6c558746518d578710a7a68732494f
Mitchell Wright <pilotmitchw@gmail.com>
2012-03-28 19:16:00 -0600

    Starting over. For the last time. I cannot use authlogic.

--- gamez/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Gamez::Application.config.secret_token = 'c944508d81772fb82984da8ad73a555834ecc861a1184085b7960396601b7b3b0adb7138bff88ac8dd5f8dd5ceb96d173da1691265db4166b73805cdf7340be6'
