af2ab117d42c45a0539c897239ec24b423ce6a77
Mitchell Wright <pilotmitchw@gmail.com>
2012-03-30 20:09:58 -0600

    Working with CanCan to implement roles.

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  ROLES = %w[admin member guest]
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable, :timeoutable, :confirmable, :activatable
   devise :database_authenticatable, :registerable,
