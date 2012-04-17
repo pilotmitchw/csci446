ae3a80c97484f475b750753d7a6b8dbb680f9f41
Mitchell Wright <pilotmitchw@gmail.com>
2012-04-17 15:50:35 -0600

    Committing commitment without fear of commitment. Also, user input accepted.

--- project07/main.js
+++ project07/main.js
@@ -2,6 +2,7 @@ var guessesLeft = 10;
 var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
 var number = 0;
 
+
 $(function() {
   updateScore(guessesLeft);
   populateHighScores(highScores);
@@ -9,14 +10,9 @@ $(function() {
 
 function generateRandomNumber() {
 	number = Math.floor( Math.random()*100 + 1 );
-
-
 }
 
-function test() {
-	generateRandomNumber();
-	alert( number );
-}
+
 
 function populateHighScores( scores ) {
   for ( var i = 0; i < scores.length; ++i ) {
@@ -24,10 +20,33 @@ function populateHighScores( scores ) {
   }
 }
 
+function test() {
+	generateRandomNumber();
+	alert( number );
+}
+
 function updateScore( score ) {
+  if ( score == 0 )
+  {
+    //Game over, you died. RIP you.
+  }
+
+  $('h2#score span#guessesLeft').empty();
   $('h2#score span#guessesLeft').append(score);
 }
 
-function guess( guess ) {
+function guessNumber() {
+	//alert("ALERT!");
 
+  the_guess = $('form#guessTheNumber input#guess').val();
+  alert( the_guess )
+  
+  if( the_guess == number )
+  {
+    
+  }
+
+
+	--guessesLeft;
+	updateScore( guessesLeft );
 }
