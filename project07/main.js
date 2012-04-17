2453f23cc2dca0c64d5e62d52c9db5ac7bcef7a3
Mitchell Wright <pilotmitchw@gmail.com>
2012-04-17 15:06:09 -0600

    Generating random number.

--- project07/main.js
+++ project07/main.js
@@ -1,17 +1,33 @@
 var guessesLeft = 10;
 var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
+var number = 0;
 
 $(function() {
   updateScore(guessesLeft);
   populateHighScores(highScores);
 });
 
-function populateHighScores(scores) {
-  for (var i = 0; i < scores.length; ++i) {
+function generateRandomNumber() {
+	number = Math.floor( Math.random()*100 + 1 );
+
+
+}
+
+function test() {
+	generateRandomNumber();
+	alert( number );
+}
+
+function populateHighScores( scores ) {
+  for ( var i = 0; i < scores.length; ++i ) {
     $('div#highScores').append("<p>" + scores[i][0] + " " + scores[i][1] + "</p>");
   }
 }
 
-function updateScore(score) {
+function updateScore( score ) {
   $('h2#score span#guessesLeft').append(score);
+}
+
+function guess( guess ) {
+
 }
